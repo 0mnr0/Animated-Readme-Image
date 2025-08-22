b9570568d808ed94dda3bbd8c42e40c92eb8feca
JustDev <savelijln0@gmail.com>
2025-08-22 21:41:11 +0400

    This Code can now be imported for the main server

---
+++ solo_launcher.py
@@ -0,0 +1,12 @@
+from flask import *
+import flask_cors
+
+app = Flask(__name__, static_folder='web/static', template_folder='web')
+flask_cors.CORS(app)
+
+import readmeServerCode
+readmeServerCode.importApp(app)
+
+
+
+app.run(port=7777, debug=True, use_reloader=False)
